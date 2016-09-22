07a06aa4896ab31466429409d7c56af750dc735f
Madhukubakaddi <madhusk@gmail.com>
2016-09-22 14:14:34 -0700

    Update Avg_nums.py

--- python/Avg_nums.py
+++ python/Avg_nums.py
@@ -1,5 +1,7 @@
 #!/tools/bin/python
 # Program to read user input of numbers, exit on 'Done' and return the average of numbers
+# This program uses the following concepts learnt in class
+# 1. print 2. variable assignment 3. 
 
 def catch_error(num):
   try :
@@ -11,6 +13,7 @@ def catch_error(num):
 
 total = 0
 count = 0
+smallest = None
 while True:
   num = raw_input("Enter number:")
   if num == 'Done':
@@ -18,8 +21,13 @@ while True:
   if catch_error(num) is None :
     continue
   else :
-    total = total + float(num)
+    num = float(num)
+    if smallest is None :
+      smallest = num
+    elif num < smallest :
+      smallest = num
+    total = total + num
     count = count + 1
 
 print "Average is:", total/count
-
+print "Smallest number is:", smallest
