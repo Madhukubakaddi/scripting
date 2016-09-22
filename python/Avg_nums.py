1ce4f418ce3fb891c14dc6f71048fc916b6c72ad
Madhukubakaddi <madhusk@gmail.com>
2016-09-22 13:53:28 -0700

    Update Avg_nums.py

--- python/Avg_nums.py
+++ python/Avg_nums.py
@@ -1,4 +1,25 @@
 #!/tools/bin/python
 # Program to read user input of numbers, exit on 'Done' and return the average of numbers
 
+def catch_error(num):
+  try :
+    num = float(num)
+  except:
+    print "Not a number"
+    return
+  return num
+
+total = 0
+count = 0
 while True:
+  num = raw_input("Enter number:")
+  if num == 'Done':
+    break
+  if catch_error(num) is None :
+    continue
+  else :
+    total = total + num
+    count = count + 1
+
+print "Average is:", total/count
+
