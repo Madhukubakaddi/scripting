3e292597d9a7c06f4c23ad3eb19101d454e3c00d
Madhukubakaddi <madhusk@gmail.com>
2016-10-07 23:16:47 -0700

    Update custome_pkts.py

--- scapy/custome_pkts.py
+++ scapy/custome_pkts.py
@@ -14,5 +14,23 @@ class MPLS(Packet):
 
 bind_layers(Ether, MPLS, type=0x8847)
 
+class VNTAG(Packet): 
+   name = "VNTAG" 
+   fields_desc =  [ BitField("dir", 0, 1), 
+                    BitField("ptr", 0, 1), 
+                    BitField("dst_vif", 0x123, 14), 
+                    BitField("l", 0, 1),
+                    BitField("rsvd", 0, 1), 
+                    BitField("ver", 0, 2), 
+                    BitField("src_vif", 0, 12) ] 
+   
+class ETAG(Packet): 
+   name = "ETAG" 
+   fields_desc =  [ BitField("pcp", 0, 3), 
+                    BitField("de", 0, 1), 
+                    BitField("svid", 0x123, 12), 
+                    BitField("rr", 0, 2),
+                    BitField("vid", 0, 14)]    
+
 label = MPLS()
 print hexdump(str(a))
