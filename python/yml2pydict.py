b6aa54ab53a8fe83163cb6a7ce969a6bb5baa3cd
Madhukubakaddi <Madhukubakaddi@users.noreply.github.com>
2017-04-21 08:19:29 -0700

    Read multiple document yml file

--- python/yml2pydict.py
+++ python/yml2pydict.py
@@ -2,9 +2,22 @@
 # Program to convert yml file to python dictionary
 
 import yaml
+#Creating a file containing variable dict which can be used by downstream files importing pydict.py module
+fh = open("pydict.py",'w')
+fh.write("dict={")
 with open("yamlfile.yml", 'r') as stream:
     try:
-        x = (yaml.load(stream))
+        #using load_all instead of load if the yml file contains more than 1 document which is represented by "---"
+        docs = yaml.load_all(stream)
     except yaml.YAMLError as exc:
         print(exc)
-
+    #Each document thus created is a dictionary.
+    for doc in docs:
+        for k,v in doc.items():
+            # writing the (k,v) in the dictionary format
+            fh.write('\'' + k '\':' + str(v) + ',')
+    #Moving 1byte from the end of file it remove the "," at the the end of dictionary.
+    fh.seek(-1,2)
+    fh.write("}")
+    fh.close()
+            
